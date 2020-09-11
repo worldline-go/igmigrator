c652ca04fc7f47588616bd425b988b97b4536a67
Vamshinandan Mekala <vamshinandan.mekala@epay.ingenico.com>
2020-09-11 12:44:01 +0200

    Update igmigrator.go

--- igmigrator.go
+++ igmigrator.go
@@ -4,8 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
-	//"gitlab.test.igdcs.com/finops/nextgen/utils/db/dbhelper.git"
-	//"gitlab.test.igdcs.com/finops/nextgen/utils/db/query/builder.git"
 
 	"io/ioutil"
 	"os"
@@ -32,15 +30,15 @@ type igMigrator struct {
 type igMigratorer interface {
 	Migrate() error
 	lockMigration() error
-	doMigrate(tx *sql.Tx,version int, filePath string) error
+	doSingleMigrate(tx *sql.Tx, version int, filePath string) error
 	setSchema()
 	getLastVersion() (int, error)
 }
 
-func NewIgMigrator(ctx context.Context,db *sqlx.DB, migrationsDir string, schemaParam string) (igMigrator,error) {
+func NewIgMigrator(ctx context.Context, db *sqlx.DB, migrationsDir string, schemaParam ...string) (igMigrator, error) {
 	var schema string
 	if len(schemaParam) > 0 {
-		schema = schemaParam
+		schema = schemaParam[0]
 	}
 	return igMigrator{
 		ctx:           ctx,
@@ -51,7 +49,6 @@ func NewIgMigrator(ctx context.Context,db *sqlx.DB, migrationsDir string, schema
 }
 
 // Migrate will run SQL files in sequence till latest version
-// Only supports up migrations for now
 func (i igMigrator) Migrate() error {
 	dir, err := os.Open(i.migrationsDir)
 	if err != nil {
@@ -86,7 +83,7 @@ func (i igMigrator) Migrate() error {
 		return err
 	}
 
-    // Lock the migration table, so that operations from other connections are blocked
+	// Lock the migration table, so that operations from other connections are blocked
 	err = i.lockMigration(tx)
 	if err != nil {
 		return err
@@ -108,24 +105,20 @@ func (i igMigrator) Migrate() error {
 		if version <= lastVersion {
 			continue
 		}
-		// versions = append(versions, version)
 		versionFiles = append(versionFiles, fileName)
 	}
 	sort.Strings(versionFiles)
 
-	log.Info().Msg(fmt.Sprintf("current_db_version : %v", lastVersion))
+	log.Info().Msgf("current db_version : %v", lastVersion)
 	newVersion := lastVersion
 	for k, value := range versionFiles {
 		fileName := value
 		newVersion = lastVersion + k + 1
-		fmt.Println(newVersion)
 		log.Info().
 			Int("updating_to_version", newVersion).
 			Str("file_name", fileName).
 			Msg("updating DB to new version")
-		fmt.Println(tx, newVersion, path.Join(i.migrationsDir, fileName))
-		if err := i.doMigrate(tx, newVersion, path.Join(i.migrationsDir, fileName)); err != nil {
-			log.Error().Err(err).Str("file_name", fileName).Msg("")
+		if err := i.doSingleMigrate(tx, newVersion, path.Join(i.migrationsDir, fileName)); err != nil {
 			return errors.Wrap(err, fmt.Sprintf("failed to run migration '%s'", fileName))
 		}
 	}
@@ -134,9 +127,9 @@ func (i igMigrator) Migrate() error {
 		return err
 	}
 	if lastVersion < newVersion {
-		log.Info().Msg(fmt.Sprintf("updated db version to :%v", newVersion))
+		log.Info().Msgf("updated db version to :%v", newVersion)
 	} else if lastVersion == newVersion {
-		log.Info().Msgf(fmt.Sprintf("no change in db verion"))
+		log.Info().Msg("no change in db version")
 	}
 	return nil
 }
@@ -154,42 +147,46 @@ func (i igMigrator) setSchema() error {
 	return nil
 }
 
-func (i igMigrator) doMigrate(tx *sql.Tx, version int, filePath string) error {
-	content, err := ioutil.ReadFile(filePath)
+// doSingleMigrate executes a single migration
+func (i igMigrator) doSingleMigrate(tx *sql.Tx, version int, filePath string) error {
+	migration, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		return errors.Wrap(err, fmt.Sprintf("failed to read file '%s'", filePath))
+		log.Info().Msgf("failed to  read file :%s", filePath)
+		return err
 	}
 
-	_, err = tx.Exec(string(content))
+	_, err = tx.Exec(string(migration))
 	if err != nil {
 		return err
 	}
-	//q := builder.NewQuery("PostgreSQL", "insert")
-	//q.Into(i.schema + ".migrations")
-	//q.InsertValue("version", version)
-	//sqlstmt, vars, err := q.Final()
-	//_, err = dbhelper.GetResults(i.db, sqlstmt, vars)
-	_,err = tx.Exec("insert into migration(version) values(?)",version)
+
+	_, err = tx.Exec("INSERT INTO migrations (version) VALUES ($1)", version)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// createMigrationTable creates the migration table if not present
+func (i igMigrator) createMigrationTable(tx *sql.Tx) error {
+	_, err := tx.Exec(`CREATE TABLE IF NOT EXISTS migrations (
+		version     INT PRIMARY KEY,
+		date	 	TIMESTAMP NOT NULL DEFAULT NOW()
+	)`)
 	return err
 }
 
-func(i igMigrator) createMigrationTable(tx *sql.Tx) error {
-   _, err := tx.Exec(`CREATE TABLE IF NOT EXISTS migrations (
-		version     INT,
-		date	 	TIMESTAMP NOT NULL DEFAULT NOW()
-	)`)
-   return err
-}
-
+// getLastVersion returns the latest migration version
 func (i igMigrator) getLastVersion(tx *sql.Tx) (int, error) {
 	var lastVersion sql.NullInt64
 	err := tx.QueryRow("SELECT MAX(version) FROM migrations").Scan(&lastVersion)
-	return int(lastVersion.Int64),err
+	return int(lastVersion.Int64), err
 }
 
+// lockMigration acquires lock on migration table so that no other parallel migration is allowed
 func (i igMigrator) lockMigration(tx *sql.Tx) error {
 	// Lock the migrations table so that other parallel migrations are blocked until current one is finished
-	_, err := tx.Exec("lock table " + i.schema + ".migrations in ACCESS EXCLUSIVE mode;")
+	_, err := tx.Exec("lock table migrations in ACCESS EXCLUSIVE mode;")
 	if err != nil {
 		return err
 	}
