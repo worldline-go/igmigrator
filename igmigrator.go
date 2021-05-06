5bcefb72a80470b9e5aa6473a5fc29405f13efe0
Yevhen Sylenko <yevhen.sylenko@epay.ingenico.com>
2021-05-06 13:30:40 +0200

    Provide more documentation on Config struct.

--- igmigrator.go
+++ igmigrator.go
@@ -50,9 +50,27 @@ type Transaction interface {
 	QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
 }
 
+// Config provides a way to specify some optional configuration.
+//
+// Most configuration options have sane defaults, which should not be changed if not specifically required.
+//
+// By default no callbacks are not set.
 type Config struct {
-	MigrationsDir  string
-	Schema         string
+	// MigrationsDir can provide a directory that will hold the migration files.
+	//
+	// By default has value of `/var/migrations/`, and should not be changed if not required.
+	// It is possible to set this value from environment variable `IGMIGRATOR_MIGRATION_DIR`
+	// if value for this variable is not set.
+	MigrationsDir string
+	// Schema can specify which schema(using `set search_path`) should be used to run migrations in.
+	//
+	// By default it will not change schema.
+	Schema string
+	// MigrationTable can provide table name for the table that will hold migrations.
+	//
+	// By default has value of 'migrations', and should not be changed if not required.
+	// It is possible to set this value from environment variable `IGMIGRATION_MIGRATION_TABLE`
+	// if value for this variable is not set.
 	MigrationTable string
 
 	// BeforeMigrationsFunc will be called after current DB version is retrieved
